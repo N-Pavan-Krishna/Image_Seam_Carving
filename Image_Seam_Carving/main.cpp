28f5541983c5266016cebb48837a46201888edbc
Pavan Krishna <n.pavankrishna96@gmail.com>
2020-10-30 20:46:35 -0400

    Initial Commit

---
+++ Image_Seam_Carving/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Image_Seam_Carving
+//
+//  Created by Pavan Krishna Nandimandalam on 2020-10-30.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
